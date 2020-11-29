db09b961110da83a744f4ba949f03f7df75d0fd2
unknown <duygumetin99@gmail.com>
2020-11-29 18:57:17 +0200

    Inheritance - lecture

---
+++ 01. Inheritance - Lab/CustomRandomList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("1.");
+            list.Add("2.");
+            list.Add("3.");
+            list.Add("4.");
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.Count);
+        }
+    }
+}
