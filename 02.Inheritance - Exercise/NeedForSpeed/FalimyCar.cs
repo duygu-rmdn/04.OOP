1b51af715a4a6a6b5e78664072d977ae7b659bbc
unknown <duygumetin99@gmail.com>
2020-11-29 23:53:56 +0200

    Inheritance - Exercise

---
+++ 02.Inheritance - Exercise/NeedForSpeed/FalimyCar.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horesePower, double fuel)
+            : base(horesePower, fuel)
+        {
+
+        }
+    }
+}
