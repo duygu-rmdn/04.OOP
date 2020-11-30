1f777bc66be7ea1521a097ec9663df0238c61097
unknown <duygumetin99@gmail.com>
2020-11-30 23:47:46 +0200

    Ecapsulation - ex - part 1

---
+++ 04.Encapsulation - Exercise/AnimalFarm/03. Shopping Spree/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03._Shopping_Spree.Models
+{
+    class Person
+    {
+    }
+}
---
+++ 04.Encapsulation - Exercise/AnimalFarm/03. Shopping Spree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Shopping_Spree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
