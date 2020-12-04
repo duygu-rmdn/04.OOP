91d18817c7f4e85fa3db7d1594362e0c1421b3ac
unknown <duygumetin99@gmail.com>
2020-12-04 21:00:01 +0200

    Polymorphism - lab

---
+++ 07.Polymorphism - Lab/Shapes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(4, 5);
+            Shape circle = new Circle(4);
+
+            Console.WriteLine(rectangle.Draw());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
