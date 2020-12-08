e506d66f0edbf4905ceed4ebdf069c125f2cd73c
unknown <duygumetin99@gmail.com>
2020-12-08 19:30:54 +0200

    Solid - exercise

---
+++ 10.Solid - Exercise/Logger/Models/Contracts/IError.cs
@@ -0,0 +1,12 @@
+﻿using Logger.Models.Enumerations;
+using System;
+
+namespace Logger.Models.Contracts
+{
+    public interface IError
+    {
+        DateTime DateTime { get; }
+        string Messege { get; }
+        Level level { get; }
+    }
+}
