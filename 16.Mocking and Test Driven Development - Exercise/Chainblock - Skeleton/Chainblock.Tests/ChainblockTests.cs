34fd39c1120c1e9741bd8f473c02e6633dddf110
unknown <duygumetin99@gmail.com>
2020-12-28 18:25:08 +0200

    Mocking and fakes exercese

---
+++ 16.Mocking and Test Driven Development - Exercise/Chainblock - Skeleton/Chainblock.Tests/ChainblockTests.cs
@@ -0,0 +1,32 @@
+﻿using Chainblock.Contracts;
+using Chainblock.Models;
+using NUnit.Framework;
+
+namespace Chainblock.Tests
+{
+    [TestFixture]
+    class ChainblockTests
+    {
+        
+
+        [Test]
+        [Category("Add method")]
+        public void
+            GivenTransaction_WhenAddTransactionIsCalled_ThenTransactionCountIncrease()
+        {
+            ITransaction transaction = new Transaction()
+            {
+                Id = 1,
+                From = "Duygu",
+                To = "Pesho",
+                Status = TransactionStatus.Successfull,
+                Amount = 5
+            };
+            IChainblock chainblock = new Chainblock();
+
+            chainblock.Add(transaction);
+
+            Assert.AreEqual(1, chainblock.Count);
+        }
+    }
+}
